b1f1f1483738361606cf3298a0259f9824ed82eb
zx-student <1771203023@qq.com>
2024-07-16 17:20:55 +0800

    封装axios、API接口统一管理

--- src/api/product/spu/index.ts
+++ src/api/product/spu/index.ts
@@ -1,6 +1,15 @@
 //SPU管理模块的接口
-import request from "@/utils/request";
-import type { SkuInfoData, SkuData, SpuData, HasSpuResponseData, AllTradeMark, SpuHasImg, SaleAttrResponseData, HasSaleAttrResponseData } from './type';
+import request from '@/utils/request'
+import type {
+  SkuInfoData,
+  SkuData,
+  SpuData,
+  HasSpuResponseData,
+  AllTradeMark,
+  SpuHasImg,
+  SaleAttrResponseData,
+  HasSaleAttrResponseData,
+} from './type'
 enum API {
   //获取已有的SPU的数据
   HASSPU_URL = '/admin/product/',
@@ -21,18 +30,29 @@ enum API {
   //查看某一个已有的SPU下全部售卖的商品
   SKUINFO_URL = '/admin/product/findBySpuId/',
   //删除已有的SPU
-  REMOVESPU_URL = '/admin/product/deleteSpu/'
+  REMOVESPU_URL = '/admin/product/deleteSpu/',
 }
 //获取某一个三级分类下已有的SPU数据
-export const reqHasSpu = (page: number, limit: number, category3Id: string | number) => request.get<any, HasSpuResponseData>(API.HASSPU_URL + `${page}/${limit}?category3Id=${category3Id}`);
+export const reqHasSpu = (
+  page: number,
+  limit: number,
+  category3Id: string | number,
+) =>
+  request.get<any, HasSpuResponseData>(
+    API.HASSPU_URL + `${page}/${limit}?category3Id=${category3Id}`,
+  )
 //获取全部的SPU的品牌的数据
-export const reqAllTradeMark = () => request.get<any, AllTradeMark>(API.ALLTRADEMARK_URL);
+export const reqAllTradeMark = () =>
+  request.get<any, AllTradeMark>(API.ALLTRADEMARK_URL)
 //获取某一个已有的SPU下全部商品的图片地址
-export const reqSpuImageList = (spuId: number) => request.get<any, SpuHasImg>(API.IMAGE_URL + spuId)
+export const reqSpuImageList = (spuId: number) =>
+  request.get<any, SpuHasImg>(API.IMAGE_URL + spuId)
 //获取某一个已有的SPU拥有多少个销售属性
-export const reqSpuHasSaleAttr = (spuId: number) => request.get<any, SaleAttrResponseData>(API.SPUHASSALEATTR_URL + spuId)
+export const reqSpuHasSaleAttr = (spuId: number) =>
+  request.get<any, SaleAttrResponseData>(API.SPUHASSALEATTR_URL + spuId)
 //获取全部的销售属性
-export const reqAllSaleAttr = () => request.get<any, HasSaleAttrResponseData>(API.ALLSALEATTR_URL);
+export const reqAllSaleAttr = () =>
+  request.get<any, HasSaleAttrResponseData>(API.ALLSALEATTR_URL)
 //添加一个新的SPU的
 //更新已有的SPU接口
 //data:即为新增的SPU|或者已有的SPU对象
@@ -41,19 +61,17 @@ export const reqAddOrUpdateSpu = (data: SpuData) => {
   if (data.id) {
     return request.post<any, any>(API.UPDATESPU_URL, data)
   } else {
-    return request.post<any, any>(API.ADDSPU_URL, data);
+    return request.post<any, any>(API.ADDSPU_URL, data)
   }
 }
 //添加SKU的请求方法
-export const reqAddSku = (data: SkuData) => request.post<any, any>(API.ADDSKU_URL, data);
+export const reqAddSku = (data: SkuData) =>
+  request.post<any, any>(API.ADDSKU_URL, data)
 
 //获取SKU数据
-export const reqSkuList = (spuId: number | string) => request.get<any, SkuInfoData>(API.SKUINFO_URL + spuId);
+export const reqSkuList = (spuId: number | string) =>
+  request.get<any, SkuInfoData>(API.SKUINFO_URL + spuId)
 
 //删除已有的SPU
-export const reqRemoveSpu = (spuId: number | string) => request.delete<any, any>(API.REMOVESPU_URL + spuId);
-
-
-
-
-
+export const reqRemoveSpu = (spuId: number | string) =>
+  request.delete<any, any>(API.REMOVESPU_URL + spuId)
